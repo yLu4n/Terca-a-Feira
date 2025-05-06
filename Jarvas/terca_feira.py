6ce6193294e9b88240467822b1dc61c0a801e7c0
Luan Victor <lu.victor.2005@gmail.com>
2025-05-06 16:40:37 -0300

    Reinicio do projeto do zero

--- Jarvas/terca_feira.py
+++ Jarvas/terca_feira.py
@@ -5,49 +5,13 @@ import schedule as sch
 import os
 import webbrowser
 
-from Jarvas.vscode import VsCode
-
-class TercaFeira(VsCode):
+def iniciar_caminhos():
     
-    def __init__(self):
-        super().__init__()
-        self.abrir_vscode()
-    
-    def abrir_programas_dias_da_semana(self):
-        
-        # Abre o Spotify nas minhas músicas Favoritas
-        os.startfile(TercaFeira.caminho_spotify)
-        pyautogui.click(x=272, y=589)
-
-        # Abre na Plataforma Udemy e entra no curso atual
-        webbrowser.open("https://www.udemy.com/home/my-courses/learning/")
-        TercaFeira.janela_curso_python.wait("exists", timeout=5)
-        time.sleep(7)
-        pyautogui.scroll(-350)
-        pyautogui.click(x=313, y=585, duration=0.8)
-
-        # Abre o Linkedin
-        webbrowser.open("https://www.linkedin.com/feed/")
-
-
-    def abrir_programas_final_de_semana():
-        os.startfile(TercaFeira.caminho_roblox)
-        webbrowser.open("https://www.youtube.com/")
-        os.startfile(TercaFeira.caminho_roblox)
-
-
-    # Caminhos dos aplicativos
     caminho_roblox = r"C:\Users\User\AppData\Local\Roblox\Versions\version-347f4ac346734391\RobloxPlayerBeta.exe"
     caminho_vs = r"C:\Users\User\AppData\Local\Programs\Microsoft VS Code\Code.exe"
     caminho_spotify = r"C:\Users\User\AppData\Local\Microsoft\WindowsApps\SpotifyAB.SpotifyMusic_zpdnekdrzrea0\Spotify.exe"
 
-    janela_curso_python = Desktop(backend="uia").window(title_re=".*Meu aprendizado | Udemy.*")
 
-    # Programação da Semana
-    sch.every().monday.do(abrir_programas_dias_da_semana())
-    sch.every().tuesday.do(abrir_programas_dias_da_semana())
-    sch.every().wednesday.do(abrir_programas_dias_da_semana())
-    sch.every().thursday.do(abrir_programas_dias_da_semana())
-    sch.every().friday.do(abrir_programas_dias_da_semana())
 
-    # Programaão do final de Semana
+
+janela_curso_python = Desktop(backend="uia").window(title_re=".*Meu aprendizado | Udemy.*")
